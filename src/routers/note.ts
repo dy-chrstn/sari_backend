704dd131095488175ffc79f424396ea36ed09285
Christian Andrei Dy <dy.cavd@gmail.com>
2024-04-09 21:21:20 +0800

    Sari backend title for get api

--- src/routers/note.ts
+++ src/routers/note.ts
@@ -4,6 +4,9 @@ import { registerNote, getNote, getAllNotes, updateNote, deleteNote } from "../c
 import { tokenAuth } from "../middleware/index";
 
 export default (router: express.Router) => {
+    router.get("/", (req: express.Request, res: express.Response) => {
+        res.send("Sari Backend")
+    })
     router.post("/registerNote/:id", tokenAuth, registerNote);
     router.get("/getNote/:userId/:noteId", tokenAuth, getNote);
     router.get("/getAllNotes/:userId", tokenAuth, getAllNotes);
